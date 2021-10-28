beca74cbe149e9100164b108a56b1021581d16a6
Pratik <pratikmndh@gmail.com>
2021-10-27 21:17:59 -0400

    removed button from expense item

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,10 +6,10 @@ import ExpenseDate from './ExpenseDate';
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle('Yay');
-    console.log(title);
-  };
+  // const clickHandler = () => {
+  //   setTitle('Yay');
+  //   console.log(title);
+  // };
 
   return (
     <li>
@@ -19,7 +19,7 @@ const ExpenseItem = (props) => {
           <h2>{title}</h2>
           <div className='expense-item__price'>{`$${props.amount}`}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
       </Card>
     </li>
   );
